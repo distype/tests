459e2abfff5f8d830b2a5e689380f1225447ebd6
BR88C <br88c@br88c.dev>
2022-04-21 11:39:10 -0400

    Add date ensure tests

---
+++ src/tests/data.ts
@@ -0,0 +1,26 @@
+import { configDotenv } from '../dotenv';
+
+import { Logger, wait } from '@br88c/node-utils';
+import { Client } from 'distype';
+
+configDotenv();
+
+const logger = new Logger({
+    enabledOutput: { log: [`DEBUG`, `INFO`, `WARN`, `ERROR`] },
+    sanitizeTokens: {
+        replacement: `%bot_token%`, token: process.env.BOT_TOKEN!
+    }
+});
+
+const client = new Client(process.env.BOT_TOKEN!, {
+    cache: { channels: [`name`, `permission_overwrites`] },
+    gateway: { intents: `nonPrivileged` }
+}, logger.log, logger);
+
+client.gateway.on(`SHARDS_READY`, async () => {
+    await wait(5000);
+    console.log(await client.getChannelData(process.env.TESTING_TEXT_CHANNEL!, `id`, `name`, `permission_overwrites`));
+    console.log(await client.getGuildData(process.env.TESTING_GUILD!, `id`, `name`, `owner_id`));
+});
+
+client.gateway.connect();
