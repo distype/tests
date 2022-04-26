3f0df7d1a8a18891745a53be3b8014d27ac16202
BR88C <br88c@br88c.dev>
2022-04-26 17:15:09 -0400

    Add permissions test

---
+++ src/tests/permissions.ts
@@ -0,0 +1,30 @@
+import { configDotenv } from '../dotenv';
+
+import { Logger, wait } from '@br88c/node-utils';
+import { Client, PermissionsUtils } from 'distype';
+
+configDotenv();
+
+const logger = new Logger({
+    enabledOutput: { log: [`DEBUG`, `INFO`, `WARN`, `ERROR`] },
+    sanitizeTokens: {
+        replacement: `%bot_token%`, token: process.env.BOT_TOKEN!
+    }
+});
+
+const client = new Client(process.env.BOT_TOKEN!, {
+    cache: {
+        channels: [`permission_overwrites`],
+        guilds: [`owner_id`, `roles`],
+        members: [`communication_disabled_until`, `roles`],
+        roles: [`permissions`]
+    },
+    gateway: { intents: `nonPrivileged` }
+}, logger.log, logger);
+
+client.gateway.on(`SHARDS_READY`, async () => {
+    await wait(5000);
+    console.log(PermissionsUtils.toReadable(await client.getSelfPermissions(process.env.TESTING_GUILD!, process.env.TESTING_TEXT_CHANNEL!)));
+});
+
+client.gateway.connect();
